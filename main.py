26af131f41860ecf0696792cb0e8b704b8b362de
Radu Siderias <radusiderias@yahoo.com>
2024-03-18 14:17:13 +0200

    initialised flask server and prediction endpoint

--- main.py
+++ main.py
@@ -2,32 +2,48 @@ import numpy as np
 import pandas as pd
 from model.RandomForest import RandomForest
 from sklearn.model_selection import train_test_split
+from flask import Flask, jsonify, request
 
-filename = "model/diabetes.csv"
-read_data = pd.read_csv(filename)
-
-# read_data.info()
-# print(read_data.duplicated().sum())
-# print(read_data.describe())
-
-feature_names = read_data.columns[:-1]
-data = read_data.iloc[:, :-1].to_numpy()
-target = read_data.iloc[:, -1].to_numpy()
-
-data_train, data_test, target_train, target_test = train_test_split(
-    data, target, test_size=0.2, random_state=1234
-)
-
-
-def accuracy(target_true, target_prediction):
-    return np.sum(target_true == target_prediction) / len(target_true)
-
+app = Flask(__name__)
 
 random_forest = RandomForest()
-random_forest.train(data_train, target_train)
-predictions_random_forest = random_forest.predict(data_test)
 
-acc_random_forest = accuracy(target_test, predictions_random_forest)
 
-print("\nAccuracy Random Forest: ")
-print(acc_random_forest)
+def train_model():
+    filename = "model/diabetes.csv"
+    read_data = pd.read_csv(filename)
+
+    data = read_data.iloc[:, :-1].to_numpy()
+    target = read_data.iloc[:, -1].to_numpy()
+
+    data_train, data_test, target_train, target_test = train_test_split(
+        data, target, test_size=0.2, random_state=1234
+    )
+
+    def accuracy(target_true, target_prediction):
+        return np.sum(target_true == target_prediction) / len(target_true)
+
+    random_forest.train(data_train, target_train)
+    predictions_random_forest = random_forest.predict(data_test)
+
+    acc_random_forest = accuracy(target_test, predictions_random_forest)
+
+    print("\nModel accuracy: ")
+    print(acc_random_forest)
+    print("\n")
+
+
+@app.route("/diabetes-prediction", methods=['POST'])
+def diabetes_prediction():
+    input_data = request.get_json()
+    input_df = pd.DataFrame([input_data]).to_numpy()
+
+    prediction = random_forest.predict(input_df)
+
+    return jsonify(prediction.tolist())
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        train_model()
+        app.run()
